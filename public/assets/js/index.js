9acfc2fff9e12e672f1733cf3a713e522b20b604
Chuhan Jie <jiechuhan1996@gmail.com>
2019-10-20 21:38:19 -0700

    miss one step

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -7,50 +7,21 @@ var $noteList = $(".list-container .list-group");
 // activeNote is used to keep track of the note in the textarea
 var activeNote = {};
 
-let ogNote
-let newNote
 
 // A function for getting all notes from the db
 var getNotes = function () {
-    $.ajax({
-        url: "/api/notes",
-        type: 'GET',
-        async: false,
-        success: function (noteData) {
-            ogNote = noteData;
-            console.log(ogNote);
-        }
-    });
-    return ogNote;
+    return $.get('/api/notes');
 };
 
 
 // A function for saving a note to the db
 var saveNote = function (note) {
-    var newNote = {
-        title: $noteTitle.val(),
-        text: $noteText.val()
-    };
-    $.ajax({
-        url: "/api/notes",
-        method: "POST",
-        data: newNote,
-        success: function (data) {
-            newNote = data;
-        }
-    })
-    console.log(newNote)
-    return newNote;
-
-    // $.post("/api/notes", newNote, function (data) {
-    //     if (data) {
-    //         console.log(data)
-    //         // alert("note added")
-    //         ogNote = data
-    //         console.log(ogNote)
-    //     };
-    // });
-    // return ogNote
+    $.post("/api/notes", note, function (data) {
+        if (data) {
+            console.log(data);
+            renderNoteList(data);
+        };
+    });
 };
 
 
@@ -58,103 +29,106 @@ var saveNote = function (note) {
 var deleteNote = function (title) {
     $.ajax({
         url: "/api/notes",
-        method: "DELETE"
-    }).then(function (noteData) {
-
-    })
+        method: "DELETE",
+        data: title
+    }).then(function (data) {
+        if (data) {
+            console.log(data);
+            renderNoteList(data);
+        };
+    });
 };
+
 
 // If there is an activeNote, display it, otherwise render empty inputs
 var renderActiveNote = function () {
-    if (activeNote !== null) {
-        $noteTitle = activeNote.title;
-        $noteText = activeNote.text;
+    if (activeNote) {
+        $noteTitle.val(activeNote.title);
+        $noteText.val(activeNote.text);
     } else {
         $noteTitle.val("");
         $noteText.val("");
-    }
+    };
 };
 
 // Get the note data from the inputs, save it to the db and update the view
 var handleNoteSave = function () {
-    let newNote = saveNote()
+    activeNote = {
+        title: $noteTitle.val(),
+        text: $noteText.val()
+    }
+    saveNote(activeNote)
     $saveNoteBtn.css("display", "none");
     $noteTitle.val("");
     $noteText.val("");
-
-    // console.log(newNote)
-    let item = $("<li>").attr({ "class": "list-group-item" })
-    let btn = $("<button>").attr("class", "delete-note")
-    item.text(newNote.title);
-    item.append(btn)
-    $(".list-group").append(item);
 };
 
 // Delete the clicked note
 var handleNoteDelete = function (event) {
     event.preventDefault();
-
-
+    var activeNote = {
+        title: $noteTitle.val(),
+        text: $noteText.val()
+    }
+    $(this).remove();
+    deleteNote(activeNote.title);
 };
 
 // Sets the activeNote and displays it
 var handleNoteView = function () {
     var title = $(this).text();
-    console.log(title)
-    console.log(ogNote)
-    for (i = 0; i < ogNote.length; i++) {
-        if (title = ogNote[i].title) {
-            console.log(ogNote[i].title)
-            return ogNote.text
-        }
-    }
-    // console.log(newNote.text)
-    // activeNote = {
-    //     title: title
-    //     // text: 
-    // }
-
-    // console.log(JSON.stringify(activeNote.title))
-
-    $noteTitle.text(title)
+    console.log(title);
+    $.get('/api/notes', function (data) {
+        console.log(data)
+        for (i = 0; i < data.length; i++) {
+            if (data[i].title === title) {
+                activeNote.title = data[i].title;
+                activeNote.text = data[i].text;
+                renderActiveNote();
+            };
+        };
+    });
 };
 
 // Sets the activeNote to and empty object and allows the user to enter a new note
 var handleNewNoteView = function () {
-
-    renderActiveNote()
+    activeNote = {};
+    renderActiveNote();
 };
 
 // If a note's title or text are empty, hide the save button
 // Or else show it
 var handleRenderSaveBtn = function () {
-    if ($noteText !== null) {
+    if ($noteText.val() !== null || $noteText.val() !== null) {
         $saveNoteBtn.css("display", "block");
+    } else {
+        $saveNoteBtn.css("display", "none");
     };
 };
 
+
 // Render's the list of note titles
 var renderNoteList = function (notes) {
-    let item = $("<li>").attr({ "class": "list-group-item", "id": "title" + i })
-    let btn = $("<button>").attr({"class": "delete-note fas fa-trash"})
-    // let title = $("<h4>").attr({"class": })
-    item.text(notes.title);
-    item.append(btn)
-    $(".list-group").append(item);
+    // console.log(notes)
+    $(".list-group").empty();
+    for (i = 0; i < notes.length; i++) {
+        let item = $("<li>").attr({ "class": "list-group-item d-flex", "id": "title" + i });
+        let btn = $("<button>").attr("class", "delete-note fas fas-trash ml-5");
+        let title = $("<h6>").text(notes[i].title);
+        item.append(title);
+        item.append(btn);
+        $(".list-group").append(item);
+    };
 };
+
 
 // Gets notes from the db and renders them to the sidebar
 var getAndRenderNotes = function () {
-    // let noteData = getNotes()
-    // for (i = 1; i < noteData.length; i++) {
-    //     let item = $("<li>").attr({ "class": "list-group-item", "id": "title" + i })
-    //     let btn = $("<button>").attr("class", "delete-note")
-    //     item.text(noteData[i].title);
-    //     item.append(btn)
-    //     $(".list-group").append(item);
-    // };
-    renderNoteList(getNotes())
+    getNotes().then(notes => {
+        renderNoteList(notes);
+    });
 };
+
 
 $saveNoteBtn.on("click", handleNoteSave);
 $noteList.on("click", ".list-group-item", handleNoteView);
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,32 +1,68 @@
 const fs = require("fs")
+const path = require("path");
 
 var noteData;
 
-module.exports = function(app) {
-    fs.readFile("./db/db.json", "utf8", function(err, data) {
+module.exports = function (app) {
+    fs.readFile("./db/db.json", "utf8", function (err, data) {
         if (err) throw err;
         noteData = JSON.parse(data);
     })
 
-    // console.log('app')
-    app.get("/api/notes", function(req, res) {
+    app.get("/api/notes", function (req, res) {
         // console.log('in get')
         // console.log(noteData)
-        res.json(noteData)
+        res.json(noteData);
     });
 
 
-    app.post("/api/notes", function(req, res) {
+    app.post("/api/notes", function (req, res) {
         var newNote = req.body;
         noteData.push(newNote);
-        console.log(noteData);
+        let parsedata = JSON.stringify(newNote)
+        fs.writeFile(path.join(__dirname, '../db/db.json'), parsedata, (err) => {
+            if (err) throw err;
+            console.log("Delete success");
+        })
+        // console.log(noteData);
         res.json(noteData);
+    });
+
+    app.delete("/api/notes", function (req, res) {
+        // console.log(req.body)
+        var deleteData = req.body
+        // console.log(deleteData)
+        console.log(noteData)
+        for (i=0; i<noteData.length; i++) {
+            // console.log(noteData[i])
+            if (deleteData.title === noteData[i].title) {
+                noteData.splice(i, 1)
+            };
+        };
+        let parsedata = JSON.stringify(noteData)
+        fs.writeFile(path.join(__dirname, '../db/db.json'), parsedata, (err) => {
+           if (err) throw err;
+           console.log("Delete success");
+       })
+        console.log(noteData)
+        res.json(noteData)
     })
 
-    // app.delete("/api/notes", function(req, res) {
-    //     if () {
-    //         noteData.splice(index, 1);
-    //         res.json(noteData)
-    //     }
+    // } else if (deleteData.title === noteData[i].title && noteData[i] === 0) {
+            //     noteData.shift(noteData[i])
+            // } else if (deleteData.title === noteData[i].title && noteData[i] === noteData.length -1) {
+            //     noteData.pop(noteData[i])
+
+
+
+    // app.delete("/api/notes/:title", function (req, res) {
+    //     let filteredarray = noteData.filter(x => {x.title !== req.params.title});
+    //     console.log(filteredarray)
+    //     let parsedata = JSON.stringify(filteredarray);
+    //     fs.writeFile("./db/db.json", parsedata, (err) => {
+    //         if (err) throw err;
+    //         console.log("Delete success");
+    //     })
+    //     res.json(filteredarray);
     // })
 }
